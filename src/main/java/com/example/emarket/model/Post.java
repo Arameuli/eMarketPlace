09e54f98a851276bb86bafd14095516d9f472e96
nika-2007 <nikasokhadze2007@gmail.com>
2024-05-27 01:16:29 +0400

    commit N2

--- src/main/java/com/example/emarket/model/Post.java
+++ src/main/java/com/example/emarket/model/Post.java
@@ -1,25 +1,25 @@
 package com.example.emarket.model;
 
 import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import java.util.Date;
 
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
 @Entity
-@Table(name = "posts")
+@Table(name ="posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
-
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    @Column(name = "postId")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "postid")
     private Integer postId;
 
-    @Column(name = "postTitle")
+    @Column(name = "posttitle")
     private String postTitle;
 
     @Column(name = "photo_url")
@@ -31,43 +31,4 @@ public class Post {
     @Column(name = "post_date")
     private Date post_date;
 
-    public Integer getPostId() {
-        return postId;
-    }
-
-    public void setPostId(Integer postId) {
-        this.postId = postId;
-    }
-
-    public String getPostTitle() {
-        return postTitle;
-    }
-
-    public void setPostTitle(String postTitle) {
-        this.postTitle = postTitle;
-    }
-
-    public String getPhoto_url() {
-        return photo_url;
-    }
-
-    public void setPhoto_url(String photo_url) {
-        this.photo_url = photo_url;
-    }
-
-    public String getDiscription() {
-        return discription;
-    }
-
-    public void setDiscription(String discription) {
-        this.discription = discription;
-    }
-
-    public Date getPost_date() {
-        return post_date;
-    }
-
-    public void setPost_date(Date post_date) {
-        this.post_date = post_date;
-    }
 }
---
+++ src/main/java/com/example/emarket/service/ModelConverter.java
@@ -0,0 +1,8 @@
+package com.example.emarket.service;
+
+import com.example.emarket.dto.PostDto;
+import com.example.emarket.model.Post;
+
+public class ModelConverter {
+
+}
